feb38ec25d82357ac83b9cd01db51ea06a6a2eeb
Mohammed Taqi <modtaqi143@gmail.com>
2024-10-29 18:00:29 +0530

    Added 3 more pattern solutions

--- Pattern solutions.js
+++ Pattern solutions.js
@@ -39,3 +39,90 @@ function printStarsTwo(n){
 };
 
 // console.log(printStarsTwo(3))
+
+///////////////////////////////////////////////////////////////////////
+
+// Pattern 3
+
+// 1
+// 1 2 
+// 1 2 3
+
+function printNumbers(n){
+  for(let i=1; i<=n; i++) {
+    let numberString = '';
+    let number = 1;
+    for(let j=1; j<=i; j++) {
+      numberString += number++;
+    }
+    console.log(numberString)
+  }
+  return '';
+}
+
+// console.log(printNumbers(3));
+
+///////////////////////////////////////////////////////////////////////
+
+// Pattern 4
+
+// 1
+// 22
+// 333
+
+function printNumbersTwo(n){
+  for(let i=1; i<=n; i++) {
+    let numberString = '';
+    let number = 1;
+    for(let j=1; j<=i; j++) {
+      numberString += i;
+    }
+    console.log(numberString)
+  }
+  return '';
+}
+
+// console.log(printNumbersTwo(3));
+
+///////////////////////////////////////////////////////////////////////
+
+// Pattern 5
+
+// * * *
+// * *
+// *
+
+function printStarsInReverseTriangle(n) {
+  for(let i=0; i<=n; i++) {
+    let string = '';
+    for(let j=1; j<=n-i; j++){
+      string += '* ';
+    }
+    console.log(string);
+  }
+  return '';
+}
+
+// console.log(printStarsInReverseTriangle(3));
+
+///////////////////////////////////////////////////////////////////////
+
+// Pattern 6
+
+// 1 2 3
+// 1 2
+// 1
+
+function printStarsInReverseTriangle(n) {
+  for(let i=0; i<=n; i++) {
+    let num = 1;
+    let string = '';
+    for(let j=1; j<=n-i; j++){
+      string += num++;
+    }
+    console.log(string);
+  }
+  return '';
+}
+
+// console.log(printStarsInReverseTriangle(3));
